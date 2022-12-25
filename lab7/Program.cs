854fa41ea9cb470428616dbb20b2a8c9a7cbcff2
Onegodx <113604166+Onegodx@users.noreply.github.com>
2022-12-25 20:29:40 +0300

    Update Program.cs

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Миньков Владимир 22исп2-1 Базовый уровень
+public class Program
+{
+    public static void Main()
+    {
+        List<Employee> employees = new List<Employee>() {
+            new Employee() { LastName = "Eddie", Age = 32, Position = "Junior Dev.", Degree = Employee.Education.Master  },
+            new Employee() { LastName = "Andrew", Age = 27, Position = "Middle Dev.", Degree = Employee.Education.Master  },
+            new Employee() { LastName = "Maria", Age = 35, Position = "Middle Dev.", Degree = Employee.Education.None  },
+            new Employee() { LastName = "Thomas", Age = 40, Position = "Middle Dev.", Degree = Employee.Education.None  },
+            new Employee() { LastName = "Alexandra", Age = 23, Position = "Junior Dev.", Degree = Employee.Education.None  }
+        };
+
+        var query = from e in employees where e.Age > 30 && e.Degree == Employee.Education.None select e;
+        foreach (var item in query)
+        {
+            Console.WriteLine(string.Format("Last Name = {0} \t Position = {1} \t Age = {2} \t Degree = {3}", item.LastName, item.Position, item.Age, item.Degree));
+        }
+    }
+}
+
+struct Employee
+{
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+    public Education Degree { get; set; }
+
+    public enum Education { None, Bachelor, Master }
+}
